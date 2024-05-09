f765ef071d59a26502df7d8b93ff741b31828b7c
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-05-09 09:46:09 +0300

    fix: use `vim.v.count` if not empty
    asdf

--- lua/langmapper/utils.lua
+++ lua/langmapper/utils.lua
@@ -220,7 +220,8 @@ end
 local function feed_nmap(keys)
   keys = vim.api.nvim_replace_termcodes(keys, true, true, true)
   -- Mode always should be noremap to avoid recursion
-  vim.api.nvim_feedkeys(keys, 'n', true)
+  local count = vim.v.count > 0 and vim.v.count or ''
+  vim.api.nvim_feedkeys(count .. keys, 'nix', true)
 end
 
 local function collect_variant_commands(from, to)
